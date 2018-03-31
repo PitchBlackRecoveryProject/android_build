4b49bd40116f275a10cb86418b92e2a1af7c8cb0
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-03-31 11:27:28 +0530

    Added Build Script to the Build Environment
    Added Build Script to Directly make Flashable Zip of PitchBlack TWRP Recovery
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -671,7 +671,10 @@ HOST_JDK_TOOLS_JAR := $(ANDROID_JAVA8_HOME)/lib/tools.jar
 
 APICHECK_COMMAND := $(JAVA) -Xmx4g -jar $(APICHECK) --no-banner --compatible-output=yes
 
-# Boolean variable determining if the allow list for compatible properties is enabled
+PB_SCRIPT := vendor/pb/pb_build.sh
+BASH := bash
+
+# Boolean variable determining if the whitelist for compatible properties is enabled
 PRODUCT_COMPATIBLE_PROPERTY := false
 ifneq ($(PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE),)
   PRODUCT_COMPATIBLE_PROPERTY := $(PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE)
