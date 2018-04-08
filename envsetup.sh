46bc98f35e8762169f10ff1af69f9d12f37291b3
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-04-08 05:26:58 +0000

    Fix TIme Lapse

--- envsetup.sh
+++ envsetup.sh
@@ -1572,6 +1572,9 @@ function make()
     else
         echo -n -e "${color_failed}#### make failed to build some targets "
     fi
+export HOURS=$hours
+export MINS=$mins
+export SECS=$secs
     if [ $hours -gt 0 ] ; then
         printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
     elif [ $mins -gt 0 ] ; then
