6c7c0e2a207005cf62a61f49a24ea9ce93f82364
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-11-04 01:25:17 +0530

    build: Export AB Details

--- envsetup.sh
+++ envsetup.sh
@@ -677,6 +677,7 @@ function lunch()
     export PB_OFFICIAL_CH=$(get_build_var PB_OFFICIAL)
     export PB_GO=$(get_build_var PB_GO)
     export PB_FORCE_DD_FLASH=$(get_build_var PB_FORCE_DD_FLASH)
+    export AB_OTA_UPDATER=$(get_build_var AB_OTA_UPDATER)
     export TARGET_PRODUCT=$product
     export TARGET_BUILD_VARIANT=$variant
     export TARGET_BUILD_TYPE=release
