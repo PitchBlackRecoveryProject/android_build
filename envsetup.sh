5f39239ebdfc96159da52088416872a7945c800d
Mohd Faraz <androiabledroid@gmail.com>
2019-04-01 21:42:00 +0000

    Build: export Device Details
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -737,6 +737,8 @@ function lunch()
 
     export PB_OFFICIAL_CH=$(get_build_var PB_OFFICIAL)
     export AB_OTA_UPDATER=$(get_build_var AB_OTA_UPDATER)
+    export TARGET_DEVICE=$(get_build_var PRODUCT_MODEL)
+    export TARGET_VENDOR=$(get_build_var PRODUCT_BRAND)
     export TARGET_PRODUCT=$product
     export TARGET_BUILD_VARIANT=$variant
     export TARGET_PLATFORM_VERSION=$(get_build_var TARGET_PLATFORM_VERSION)
