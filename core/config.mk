41e9a213b407c6885880cbc16f8f8bbc5a02bb26
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-06-30 09:01:00 +0000

    PBRP: use pb
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -230,7 +230,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
--include vendor/omni/config/BoardConfigOmni.mk
+-include vendor/pb/config/BoardConfigOmni.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
