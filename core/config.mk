8592fb949af8a473de25c5751a080e699e3b7e27
Mohd Faraz <androiabledroid@gmail.com>
2021-04-12 23:03:25 +0200

    Revert "Added Build Script to the Build Environment"
    This reverts commit 4b49bd40116f275a10cb86418b92e2a1af7c8cb0.

--- core/config.mk
+++ core/config.mk
@@ -671,10 +671,7 @@ HOST_JDK_TOOLS_JAR := $(ANDROID_JAVA8_HOME)/lib/tools.jar
 
 APICHECK_COMMAND := $(JAVA) -Xmx4g -jar $(APICHECK) --no-banner --compatible-output=yes
 
-PB_SCRIPT := vendor/pb/pb_build.sh
-BASH := bash
-
-# Boolean variable determining if the whitelist for compatible properties is enabled
+# Boolean variable determining if the allow list for compatible properties is enabled
 PRODUCT_COMPATIBLE_PROPERTY := false
 ifneq ($(PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE),)
   PRODUCT_COMPATIBLE_PROPERTY := $(PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE)
